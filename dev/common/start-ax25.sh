99ea0286b4d3bf53c4eed3b1ba8ffea36ab659f8
Lars Kellogg-Stedman <lars@oddbit.com>
2024-04-10 08:31:35 -0400

    Use common ax.25 init script

--- dev/common/start-ax25.sh
+++ dev/common/start-ax25.sh
@@ -14,7 +14,10 @@ while ! [ -c /run/radio/ptyudp1 ]; do sleep 1; done
 ax25ipd -c /etc/ax25/ax25ipd-udp0.conf -d /run/radio/ptyudp0
 ax25ipd -c /etc/ax25/ax25ipd-udp1.conf -d /run/radio/ptyudp1
 
-kissattach -l $(readlink /run/radio/kissudp0) udp0
-kissattach -l $(readlink /run/radio/kissudp1) udp1
+kissattach -l "$(readlink /run/radio/kissudp0)" udp0
+kissattach -l "$(readlink /run/radio/kissudp1)" udp1
+
+kissparms -p udp0 -c 1
+kissparms -p udp1 -c 1
 
 ax25d -l
