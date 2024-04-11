763ffa6c5cbf57570bb4a784f8c436de5d551bbd
Lars Kellogg-Stedman <lars@oddbit.com>
2024-04-10 22:50:16 -0400

    Avoid affending mailing list people with socat

--- dev/common/start-ax25.sh
+++ dev/common/start-ax25.sh
@@ -1,21 +1,27 @@
 #!/bin/sh
 
 mkdir -p /run/radio
-socat \
-	pty,link=/run/radio/ptyudp0 \
-	pty,link=/run/radio/kissudp0 &
-socat \
-	pty,link=/run/radio/ptyudp1 \
-	pty,link=/run/radio/kissudp1 &
 
-while ! [ -c /run/radio/ptyudp0 ]; do sleep 1; done
-while ! [ -c /run/radio/ptyudp1 ]; do sleep 1; done
+#socat \
+#	pty,link=/run/radio/ptyudp0 \
+#	pty,link=/run/radio/kissudp0 &
+#socat \
+#	pty,link=/run/radio/ptyudp1 \
+#	pty,link=/run/radio/kissudp1 &
+#
+#while ! [ -c /run/radio/ptyudp0 ]; do sleep 1; done
+#while ! [ -c /run/radio/ptyudp1 ]; do sleep 1; done
 
-ax25ipd -c /etc/ax25/ax25ipd-udp0.conf -d /run/radio/ptyudp0
-ax25ipd -c /etc/ax25/ax25ipd-udp1.conf -d /run/radio/ptyudp1
+tmpfile=$(mktemp /tmp/ptyXXXXXX)
+trap 'rm -f $tmpfile' EXIT
 
-kissattach -l "$(readlink /run/radio/kissudp0)" udp0
-kissattach -l "$(readlink /run/radio/kissudp1)" udp1
+ax25ipd -c /etc/ax25/ax25ipd-udp0.conf >"$tmpfile"
+udp0_pty=$(tail -1 "$tmpfile")
+ax25ipd -c /etc/ax25/ax25ipd-udp1.conf >"$tmpfile"
+udp1_pty=$(tail -1 "$tmpfile")
+
+kissattach -l "$udp0_pty" udp0
+kissattach -l "$udp1_pty" udp1
 
 kissparms -p udp0 -c 1
 kissparms -p udp1 -c 1
